ad7ecfea508c697fb16e1e7a7155f70d8e5e0670
AhMed-222516a <ahmedadelamer25@gmail.com>
2026-02-20 19:30:55 +0200

    a

---
+++ stc/ML/session_4/code.py
@@ -0,0 +1,27 @@
+#pip install tkintertable
+import tkinter as tk
+import numpy as np
+
+class SalaryPredictionApp:
+    def __init__(self,root):
+        self.root = root
+        self.root.title("DEPI Diploma for AI")
+        self.root.geometry("500x400")
+        self.create_widgets()
+
+    def create_widgets(self):
+        header = tk.Label(self.root,text="Depi -machine learning diploma",
+                    bg='blue',fg="white",font=("Arial",28,"bold"))
+        header.pack(pady=tk.X)
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = SalaryPredictionApp(root)
+    root.mainloop()
+"""
+terminalt
+python tk_gui.py 
+"""
+
